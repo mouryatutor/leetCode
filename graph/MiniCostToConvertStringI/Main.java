0cdca2d4429be40fa6f8912836cd10cd630036c7
mouryatutor <156491972+mouryatutor@users.noreply.github.com>
2024-02-13 00:54:32 +0000

    MiniCostToConvertString is pending

--- graph/MiniCostToConvertStringI/Main.java
+++ graph/MiniCostToConvertStringI/Main.java
@@ -6,52 +6,44 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         String source = "abcd";
-        String target = "abce";
-        char[] original = {'a','c'};
-        char[] changed = {'c','b'};
-        int[] cost = {1,2};
-        Map<Character,node> distanceHistories =  new HashMap<>();
+        String target = "acbe";
+        char[] original = {'a','b','c','c','e','d'};
+        char[] changed = {'b','c','b','e','b','e'};
+        int[] cost = {1,5,2,3,5,7,11,7};
         int n = original.length;
-
         
         Map<Character,ArrayList<node>> hashMap = new HashMap<>();
 
+        for(char ch = 'a'; ch <= 'z'; ch++)
+        {
+            hashMap.put(ch, new ArrayList<>());
+        }
+
         for(int i = 0; i < n; i++)
         {
-            if(hashMap.get(original[i]) == null)
-            {
-                hashMap.put(original[i], new ArrayList<>());
-            }
             hashMap.get(original[i]).add(new node(changed[i], cost[i]));
         }
-        for(int i = 0; i < n; i++)
-        {
-            if(hashMap.get(changed[i]) == null)
-            {
-                hashMap.put(changed[i], new ArrayList<>());
-            }
-        }
+
+        // System.out.println(hashMap);
 
         int totalCost = 0;
         for(int i = 0; i < source.length(); i++)
         {
-            if(hashMap.get(source.charAt(i)) == null)
+            if(target.charAt(i) != source.charAt(i))
             {
-                totalCost = -1;
-                break;
-            }  
-            if(source.charAt(i) == target.charAt(i))
-            {
-                totalCost = totalCost + 0;
-            }else if(distanceHistories.get(source.charAt(i)) == null || (distanceHistories.get(source.charAt(i)) != null && distanceHistories.get(source.charAt(i)).chr != target.charAt(i))) {
-                Map<Character,Integer> distances = dijKstrasAlgorithm(hashMap,original,changed,new node(source.charAt(i),cost[i]));
+                Map<Character,Integer> distances = dijKstrasAlgorithm(hashMap, new node(source.charAt(i), 0));
                 System.out.println(distances);
-                char destination = target.charAt(i);
-                distanceHistories.put(source.charAt(i), new node(destination,distances.get(destination)));
-                totalCost =  totalCost + distances.get(destination);
-            }else {
-                totalCost =  totalCost + distanceHistories.get(source.charAt(i)).cost;
+                System.out.println("***********************");
+                if(distances.size() == 0 || distances.get(target.charAt(i)) == Integer.MAX_VALUE)
+                {
+                    totalCost = -1;
+                    break;
+                }else 
+                {
+                    totalCost = totalCost + distances.get(target.charAt(i));
+                }
             }
+
         }
 
         System.out.println("Total cost " + totalCost);
@@ -59,20 +51,21 @@ public class Main {
 
     }
 
-    private static Map<Character,Integer> dijKstrasAlgorithm(Map<Character,ArrayList<node>> adjList ,char[] original, char[] changed, node source)
+    private static Map<Character,Integer> dijKstrasAlgorithm(Map<Character,ArrayList<node>> adjList , node source)
     {
-        
+
         Map<Character,Integer> distance =  new HashMap<>();
         Map<Character,Boolean> visited =  new HashMap<>();
-        for(char chr: original)
+
+        if(adjList.get(source.chr).size() == 0)
         {
-           distance.put(chr, Integer.MAX_VALUE);
-           visited.put(chr, false);
+            return distance;
         }
-        for(char chr: changed)
+
+        for(char ch = 'a'; ch <= 'z'; ch++)
         {
-           distance.put(chr, Integer.MAX_VALUE);
-           visited.put(chr, false);
+           distance.put(ch, Integer.MAX_VALUE);
+           visited.put(ch, false);
         }
         
         distance.put(source.chr,0);
@@ -81,19 +74,21 @@ public class Main {
         PriorityQueue<node> pq = new PriorityQueue<>((a,b) -> a.cost - b.cost);
         pq.add(source);
         
-        while (pq.isEmpty() == false) {
+         while(pq.isEmpty() == false) {
 
             node current = pq.poll();
             visited.put(current.chr,true);
-
+            
             for(node temp: adjList.get(current.chr))
             {
+                // System.out.print(temp.chr + " ");
                 if(visited.get(temp.chr) == false)
                 {
                     distance.put(temp.chr, Math.min(distance.get(current.chr) + temp.cost, distance.get(temp.chr)));
                     pq.add(temp);
                 }
             }
+            // System.out.println();
         }
 
         return distance;
